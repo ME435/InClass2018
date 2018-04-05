0f06e3e5b679f95f4b856c81f3fdc179646c3959
David Fisher <fisherds@rose-hulman.edu>
2018-04-05 15:31:22 -0400

    In class 435 examples

--- LedToggle/app/src/main/java/edu/rosehulman/fisherds/ledtoggle/MainActivity.java
+++ LedToggle/app/src/main/java/edu/rosehulman/fisherds/ledtoggle/MainActivity.java
@@ -1,11 +1,17 @@
 package edu.rosehulman.fisherds.ledtoggle;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import edu.rosehulman.me435.AccessoryActivity;
+import edu.rosehulman.me435.TextToSpeechHelper;
+
+public class MainActivity extends AccessoryActivity {
+
+  private TextToSpeechHelper mTts;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -13,14 +19,41 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
   }
 
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mTts = new TextToSpeechHelper(this);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mTts.shutdown();
+  }
+
   public void pressedLedOn(View view) {
-    Toast.makeText(this, "LED On", Toast.LENGTH_SHORT).show();
-    // TODO: Actually implement
+    //Toast.makeText(this, "LED On", Toast.LENGTH_SHORT).show();
+    sendCommand("On");
   }
 
   public void pressedLedOff(View view) {
-    Toast.makeText(this, "LED Off", Toast.LENGTH_SHORT).show();
-    // TODO: Actually implement
+    //Toast.makeText(this, "LED Off", Toast.LENGTH_SHORT).show();
+    sendCommand("Off");
   }
 
+  @Override
+  protected void onCommandReceived(String receivedCommand) {
+    super.onCommandReceived(receivedCommand);
+    Toast.makeText(this, "Received: " + receivedCommand, Toast.LENGTH_SHORT).show();
+
+    if (receivedCommand.toLowerCase().contains("left")) {
+      findViewById(android.R.id.content).setBackgroundColor(Color.GREEN);
+      mTts.speak(receivedCommand);
+    } else if (receivedCommand.toLowerCase().contains("right")) {
+      findViewById(android.R.id.content).setBackgroundColor(Color.BLACK);
+      mTts.speak("Sure say whatever you like");
+    }
+
+
+  }
 }
