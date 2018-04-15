573bc0ab4a857288f145c5f682b9a8dc7097b2c2
David Fisher <fisherds@rose-hulman.edu>
2018-04-14 23:18:50 -0400

    Added the two button callbacks

--- FieldSensors/app/src/main/java/edu/rosehulman/fisherds/fieldsensors/MainActivity.java
+++ FieldSensors/app/src/main/java/edu/rosehulman/fisherds/fieldsensors/MainActivity.java
@@ -2,6 +2,8 @@ package edu.rosehulman.fisherds.fieldsensors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void handleSetOrigin(View view) {
+    Log.d("FS", "You clicked Set Origin");
+  }
+
+  public void handleSetXAxis(View view) {
+    Log.d("FS", "You clicked Set X axis");
   }
 }
